3f14429eb2502aec8145d4d32c5ecac74303c284
Nawin chander <nawinnapzy289@gmail.com>
2026-03-16 19:56:11 +0530

    add the code

---
+++ Advance/Sliding Window Maximum.js
@@ -0,0 +1,24 @@
+// Problem
+
+// Given an array and window size k, find maximum element in every window.
+
+// EXAMPLE
+// Input:
+// nums = [1,3,-1,-3,5,3,6,7]
+// k = 3
+
+// Output:
+// [3,3,5,5,6,7]
+
+// WINDOWS
+
+// [1 3 -1] → 3
+// [3 -1 -3] → 3
+// [-1 -3 5] → 5
+// [-3 5 3] → 5
+// [5 3 6] → 6
+// [3 6 7] → 7
+
+
+
+
