9155208c762c97926f6c4720c3a5428966d37903
Nawin chander <nawinnapzy289@gmail.com>
2026-03-16 19:56:56 +0530

    add the code

--- Advance/Sliding Window Maximum.js
+++ Advance/Sliding Window Maximum.js
@@ -19,6 +19,37 @@
 // [5 3 6] → 6
 // [3 6 7] → 7
 
+function maxSlidingWindow(nums, k) {
+
+    const deque = [];
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+
+        // remove elements outside window
+        if (deque.length && deque[0] === i - k) {
+            deque.shift();
+        }
+
+        // remove smaller elements
+        while (deque.length && nums[i] > nums[deque[deque.length - 1]]) {
+            deque.pop();
+        }
+
+        deque.push(i);
+
+        // window reached
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+
+    return result;
+}
+
+console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7],3));
+
+
 
 
 
