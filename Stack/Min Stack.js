6c7f49fd17bd338b3e81b2ffa01d096c7a993c73
Nawin chander <nawinnapzy289@gmail.com>
2026-03-21 16:25:46 +0530

    add the code

--- Stack/Min Stack.js
+++ Stack/Min Stack.js
@@ -5,6 +5,48 @@
 // One for values
 // One for tracking minimum
 
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+        if (
+            this.minStack.length === 0 ||
+            val <= this.getMin()
+        ) {
+            this.minStack.push(val);
+        }
+    }
+
+    pop() {
+        if (this.stack.pop() === this.getMin()) {
+            this.minStack.pop();
+        }
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
+
+// Example
+let minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+console.log(minStack.getMin()); // -3
+minStack.pop();
+console.log(minStack.top());    // 0
+console.log(minStack.getMin()); // -2
+
+
 
 
 
