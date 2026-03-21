5cd38ac7eed06782c65db5f602d76bc9bf586c39
Nawin chander <nawinnapzy289@gmail.com>
2026-03-21 16:28:40 +0530

    add the code

---
+++ Queue/LRU Cache (Queue + HashMap).js
@@ -0,0 +1,15 @@
+// Concept:
+
+// Use:
+
+// HashMap → O(1) access
+// Doubly Linked List (acts like a queue) → track usage order
+
+// 👉 Most recently used → front
+// 👉 Least recently used → back (to remove)
+
+// ⏱ Time: O(1) for get & put
+
+
+
+
