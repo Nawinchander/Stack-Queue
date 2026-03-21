f06e0ca1fc84745107ec33e3963e399d0399f9be
Nawin chander <nawinnapzy289@gmail.com>
2026-03-21 16:26:50 +0530

    add the code

---
+++ Stack/Trapping Rain Water.js
@@ -0,0 +1,15 @@
+// Trapping Rain Water (Stack)
+// 💡 Concept:
+
+// Use a monotonic decreasing stack to calculate trapped water between bars.
+
+// 🧠 Idea:
+
+// When you find a taller bar, you can trap water using previous smaller bars.
+
+// ⏱ Time: O(n)
+
+
+
+
+
