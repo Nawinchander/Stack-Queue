13cf4fb451330dfc3903cbeaa8608c7863e85e46
Nawin chander <nawinnapzy289@gmail.com>
2026-03-21 16:28:00 +0530

    add the code

--- Stack/Trapping Rain Water.js
+++ Stack/Trapping Rain Water.js
@@ -5,6 +5,34 @@
 
 // ⏱ Time: O(n)
 
+function trap(height) {
+    let stack = [];
+    let water = 0;
+
+    for (let i = 0; i < height.length; i++) {
+        while (stack.length && height[i] > height[stack[stack.length - 1]]) {
+            let top = stack.pop();
+
+            if (!stack.length) break;
+
+            let distance = i - stack[stack.length - 1] - 1;
+            let boundedHeight = Math.min(
+                height[i],
+                height[stack[stack.length - 1]]
+            ) - height[top];
+
+            water += distance * boundedHeight;
+        }
+        stack.push(i);
+    }
+
+    return water;
+}
+
+// Example
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])); // 6
+
+
 
 
 
