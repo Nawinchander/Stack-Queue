fef711dfc1a586510690e618690e3c43de5acc26
Nawin chander <nawinnapzy289@gmail.com>
2026-03-21 16:24:10 +0530

    add the code

--- Queue/Sliding Window Maximum.js
+++ Queue/Sliding Window Maximum.js
@@ -1,3 +1,40 @@
 //// Sliding Window Maximum (Deque / Queue)
 
 //// Use a deque (double-ended queue) to keep track of max elements.
+
+
+function maxSlidingWindow(nums, k) {
+    let deque = []; // stores indices
+    let result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+
+        // Remove out-of-window elements
+        if (deque.length && deque[0] <= i - k) {
+            deque.shift();
+        }
+
+        // Maintain decreasing order
+        while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+
+        deque.push(i);
+
+        // Window formed
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+
+    return result;
+}
+
+// Example
+console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3));
+// Output: [3,3,5,5,6,7]
+
+
+
+
+
