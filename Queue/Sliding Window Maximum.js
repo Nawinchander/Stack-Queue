ab663e62b258442fb316ede4ca0b1102efd244db
Nawin chander <nawinnapzy289@gmail.com>
2026-03-21 16:23:38 +0530

    add the code and file

---
+++ Queue/Sliding Window Maximum.js
@@ -0,0 +1,3 @@
+//// Sliding Window Maximum (Deque / Queue)
+
+//// Use a deque (double-ended queue) to keep track of max elements.
--- Stack/Largest Rectangle.js
+++ Stack/Largest Rectangle.js
@@ -1,5 +1,26 @@
 /// Largest Rectangle in Histogram (Stack)
 
+function largestRectangleArea(heights) {
+    let stack = [];
+    let maxArea = 0;
+    heights.push(0); // sentinel
+
+    for (let i = 0; i < heights.length; i++) {
+        while (stack.length && heights[i] < heights[stack[stack.length - 1]]) {
+            let h = heights[stack.pop()];
+            let w = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, h * w);
+        }
+        stack.push(i);
+    }
+
+    return maxArea;
+}
+
+// Example
+console.log(largestRectangleArea([2,1,5,6,2,3])); // 10
+
+
 
 
 
